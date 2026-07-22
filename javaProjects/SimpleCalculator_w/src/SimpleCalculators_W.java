fa73cbe730a62a1589b5b3a885721f2eb84d54ad
MuhazCodes00 <muhazoladejo48@gmail.com>
2026-07-22 15:50:51 +0100

    i updated java projects

---
+++ javaProjects/SimpleCalculator_w/src/SimpleCalculators_W.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Layi
+ */
+public class SimpleCalculators_W extends javax.swing.JFrame {
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String args[]) {
+    java.awt.EventQueue.invokeLater(() -> {
+        new SimpleCalculator_w().setVisible(true);
+    });
+}
+
+
+   
+ }
+
